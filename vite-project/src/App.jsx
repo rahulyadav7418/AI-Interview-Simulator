e4fb53acbdb9f094fb4a82c75266dd06300797e2
Rahul Yadav <yadavrahul776393@gmail.com>
2026-05-31 12:58:23 +0530

    Added InerviewCard component and learned props and map

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,19 @@
 import Navbar from "./Navbar";
 import Hero from "./Hero";
+import InterviewCard from "./InterviewCard";
+const interviews = ["Frontend Interview", "Backend Interview", "AI Interview", "Java Interview"];
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      {
+        interviews.map((item, index) => (
+          <InterviewCard key = { index }
+          title = { item } />
+        ))
+      }
     </div>
   );
 }
---
+++ vite-project/src/InterviewCard.jsx
@@ -0,0 +1,9 @@
+function InterviewCard(props) {
+    return (
+        <div>
+            <h2>{ props.title }</h2>
+            <button>Select</button>
+        </div>
+    );
+}
+export default InterviewCard;
