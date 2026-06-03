4a8173c92294126bf44faedbbdc3267dd2f0117c
Rahul Yadav <yadavrahul776393@gmail.com>
2026-06-03 22:43:12 +0530

    Added interview Question when user clicks interviewcard question will show according index and added next button after clicking next button next question will show

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,9 +4,36 @@ import InterviewCard from "./InterviewCard";
 import { useState } from "react";
 import "./App.css";
 const interviews = ["Frontend Interview", "Backend Interview", "AI Interview", "Java Interview"];
+const interviewQuestions = {
+  "Frontend Interview": [
+    "What is React?",
+    "What is JSX?",
+    "What is Virtual Dom?"
+  ],
+
+  "Backend Interview": [
+    "What is Node.js?",
+    "What is Express.js?",
+    "What is API?"
+  ],
+
+  "AI Interview": [
+    "What is AI?",
+    "What is Machine Learning?",
+    "What is Deep Learning?"
+  ],
+
+  "Java Interview": [
+    "What is OOP?",
+    "What is Inheritence?",
+    "What is Polimorphism?"
+  ],
+
+};
 
 function App() {
   const [selectedInterview, setSelectedInterview] = useState("");
+  const [currentQuestion, setCurrentQuestion] = useState(0);
   return (
     <div>
       <Navbar />
@@ -20,7 +47,30 @@ function App() {
         ))
       }
 
-      <h2>Selected Interview: { selectedInterview }</h2>
+      { selectedInterview && ( <h2> Selected Interview: {selectedInterview}</h2>
+      )}
+
+      {
+        selectedInterview && (
+          <div>
+            <h3>Question:</h3>
+
+            <p>
+              { interviewQuestions [selectedInterview] [currentQuestion] }
+            </p>
+
+            {
+              currentQuestion < interviewQuestions[selectedInterview].length - 1 && (
+                <button onClick={() =>
+                  setCurrentQuestion(currentQuestion + 1)
+                }>
+                  Next Question
+                </button>
+              )
+            }
+          </div>
+        )
+      }
 
     </div>
   );
--- vite-project/src/Hero.jsx
+++ vite-project/src/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
     const [started, setStarted] = useState(false);
     return (
         <div>
-        <h1>AI Interview Simulator</h1>
+        <h1>Mock Interview Platform</h1>
         <input type="text" placeholder="Enter your name" onChange={(e) => setName(e.target.value)}/>
         <h2>Welcome { name }</h2>
         <button onClick={() => setStarted(true)}>
